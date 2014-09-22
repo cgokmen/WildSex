a376e368cf7655139587742949bba5cac7bca50f
Cem Gökmen <cem@cemgokmen.com>
2014-09-22 14:57:39 -0400

    Pushed a variety of update-related fixes.

--- WildSex/src/main/java/com/cemgokmen/wildsex/WildSex.java
+++ WildSex/src/main/java/com/cemgokmen/wildsex/WildSex.java
@@ -51,7 +51,38 @@ public class WildSex extends JavaPlugin {
         this.autoUpdate = this.getConfig().getBoolean("auto-update");
 
         if (this.autoUpdate) {
-            Updater updater = new Updater(this, 85038, this.getFile(), Updater.UpdateType.DEFAULT, false);
+            Updater updater = new Updater(this, 85038, this.getFile(), Updater.UpdateType.DEFAULT, true);
+            getLogger().log(Level.INFO, "Running automatic updater.");
+            Updater.UpdateResult result = updater.getResult();
+            switch(result)
+            {
+                case SUCCESS:
+                    getLogger().log(Level.INFO, "Success: The updater found an update, and has readied it to be loaded the next time the server restarts/reloads");
+                    break;
+                case NO_UPDATE:
+                    getLogger().log(Level.INFO, "No Update: The updater did not find an update, and nothing was downloaded.");
+                    break;
+                case DISABLED:
+                    getLogger().log(Level.INFO, "Won't Update: The updater was disabled in its configuration file.");
+                    break;
+                case FAIL_DOWNLOAD:
+                    getLogger().log(Level.INFO, "Download Failed: The updater found an update, but was unable to download it.");
+                    break;
+                case FAIL_DBO:
+                    getLogger().log(Level.INFO, "dev.bukkit.org Failed: For some reason, the updater was unable to contact DBO to download the file.");
+                    break;
+                case FAIL_NOVERSION:
+                    getLogger().log(Level.INFO, "No version found: When running the version check, the file on DBO did not contain the a version in the format 'vVersion' such as 'v1.0'.");
+                    break;
+                case FAIL_BADID:
+                    getLogger().log(Level.INFO, "Bad id: The id provided by the plugin running the updater was invalid and doesn't exist on DBO.");
+                    break;
+                case FAIL_APIKEY:
+                    getLogger().log(Level.INFO, "Bad API key: The user provided an invalid API key for the updater to use.");
+                    break;
+                case UPDATE_AVAILABLE:
+                    getLogger().log(Level.INFO, "There was an update found, but because you had the UpdateType set to NO_DOWNLOAD, it was not downloaded.");
+            }
         }
 
         this.listener = new WildSexTaskListener(this);
